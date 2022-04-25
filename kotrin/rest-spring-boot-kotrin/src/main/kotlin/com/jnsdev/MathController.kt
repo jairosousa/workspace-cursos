0b0d2096989b3c7d90e6131bc23cb852a14384cb
Jairo <jaironsousa@gmail.com>
2022-04-25 16:36:27 -0300

    :rocket: Curso REST API's RESTFul Spring Boot, Kotlin, Docker   - :construction_worker: Entendendo as Annotations @RestController, @RequestMapping e @PathVariable

--- kotrin/rest-spring-boot-kotrin/src/main/kotlin/com/jnsdev/MathController.kt
+++ kotrin/rest-spring-boot-kotrin/src/main/kotlin/com/jnsdev/MathController.kt
@@ -1,5 +1,6 @@
 package com.jnsdev
 
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,11 +12,13 @@ import java.util.concurrent.atomic.AtomicLong
  * @Created 25/04/2022 - 11:47
  */
 @RestController
-class GreetingController {
+class MathController {
+    
     val counter: AtomicLong = AtomicLong()
 
-    @RequestMapping("/greeting")
-fun greeting(@RequestParam(value = "name", defaultValue = "Word") name: String?): Greeting {
-        return Greeting(counter.incrementAndGet(), "Hello, $name!")
+    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
+    fun sum(@PathVariable(value = "numberOne") numberOne: String?,
+            @PathVariable(value = "numberTwo") numberTwo: String?): Double {
+        return 1.0
     }
 }
