2e9de534e04ae52914811917f5c6df3b1d4fd863
Jairo <jaironsousa@gmail.com>
2022-05-15 10:09:32 -0300

    Criação do projeto  - Utilização Spring initializr  - Curso Softblue

---
+++ java/softblue/realmeet/realmeet/src/main/java/br/com/softblue/realmeet/RealmeetApplication.java
@@ -0,0 +1,13 @@
+package br.com.softblue.realmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealmeetApplication.class, args);
+	}
+
+}
---
+++ java/softblue/realmeet/realmeet/src/test/java/br/com/softblue/realmeet/RealmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softblue.realmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
