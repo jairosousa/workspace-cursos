b6a51e04d3d8d21ff3facfb87b50e1f43332585f
Jairo <jaironsousa@gmail.com>
2022-05-15 10:40:42 -0300

    Adicionando modulos no projeto maven  - service  - client

--- java/softblue/realmeet/service/src/test/java/br/com/softblue/realmeet/ApplicationTests.java
+++ java/softblue/realmeet/service/src/test/java/br/com/softblue/realmeet/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RealmeetApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
