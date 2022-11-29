bc3e5ef657986f1e8982df0bc22984f38b4d1928
Jairo Nascimento <jaironsousa@gmail.com>
2022-11-29 16:38:47 -0300

    Curso Master Microserviços and Spring Boot and Spring Cloud  - Seção: 3 Restfull webservices  	- aula: 21 Step 10 - Enhancing POST Method to return correct HTTP Status Code and Location

--- bussiness-Udemy/Microservices-with-SpringBoot-Cloud/restfull-web-services/src/main/java/com/jnsdev/rest/webservices/restfullwebservices/user/UserResource.java
+++ bussiness-Udemy/Microservices-with-SpringBoot-Cloud/restfull-web-services/src/main/java/com/jnsdev/rest/webservices/restfullwebservices/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.jnsdev.rest.webservices.restfullwebservices.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -29,9 +32,13 @@ public class UserResource {
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        var save = userDaoService.save(user);
-        return save;
-
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
