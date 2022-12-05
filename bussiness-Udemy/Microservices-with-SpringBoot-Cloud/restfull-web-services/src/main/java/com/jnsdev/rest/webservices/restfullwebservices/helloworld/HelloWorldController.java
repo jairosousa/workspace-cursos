ea34c1d448df6cfad961ca53906204563f7af835
Jairo Nascimento <jaironsousa@gmail.com>
2022-12-05 19:30:47 -0300

    Curso Master Microserviços and Spring Boot and Spring Cloud  - Seção: 3 Restfull webservices  	- aula:  19 - Exploring Internationalization for REST API

---
+++ bussiness-Udemy/Microservices-with-SpringBoot-Cloud/restfull-web-services/src/main/java/com/jnsdev/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,34 @@
+package com.jnsdev.rest.webservices.restfullwebservices.helloworld;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 05/12/2022 - 19:12
+ */
+@RestController
+public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Heelo World";
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+    }
+}
