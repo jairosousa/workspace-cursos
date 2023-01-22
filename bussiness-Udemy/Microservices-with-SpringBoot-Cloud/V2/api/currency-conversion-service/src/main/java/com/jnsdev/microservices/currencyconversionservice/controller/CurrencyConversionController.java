9df43a162ec456a0c1583a72e6e199c0abe9fd53
Jairo Nascimento <jaironsousa@gmail.com>
2023-01-22 17:37:21 -0300

    Curso Master Microservices with Spring Cloud - V2  - Seção: 6 Restfull webservices  	- aula: 147 Step Step 17 - Invoking Currency Exchange from Currency Conversion- V2

--- bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-conversion-service/src/main/java/com/jnsdev/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-conversion-service/src/main/java/com/jnsdev/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,22 +1,39 @@
 package com.jnsdev.microservices.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.jnsdev.microservices.currencyconversionservice.bean.CurrencyConversion;
 
 @RestController
 public class CurrencyConversionController {
-      
+
       @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
       public CurrencyConversion calculatorCurrencyConversion(
-            @PathVariable String from,
-            @PathVariable String to,
-            @PathVariable BigDecimal quantity) {
-          return new CurrencyConversion(1000L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+                  @PathVariable String from,
+                  @PathVariable String to,
+                  @PathVariable BigDecimal quantity) {
+
+            HashMap<String, String> uriVariables = new HashMap<>();
+            uriVariables.put("from", from);
+            uriVariables.put("to", to);
+
+            ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+                        "http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class,
+                        uriVariables);
+
+            CurrencyConversion currencyConversion = responseEntity.getBody();
+
+            return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+                        currencyConversion.getConversionMultiple(),
+                        quantity.multiply(currencyConversion.getConversionMultiple()),
+                        currencyConversion.getEnvironment());
       }
-      
+
 }
