241236169667d0c474b4988148c57d27ed34a220
Jairo Nascimento <jaironsousa@gmail.com>
2023-01-22 16:22:31 -0300

    Curso Master Microservices with Spring Cloud - V2  - Seção: 6 Restfull webservices  	- aula: 141 Step Step 13 - Configures JPA and initialized Data - V2

---
+++ bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-exchange-service/src/main/java/com/jnsdev/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,34 @@
+package com.jnsdev.microservices.currencyexchangeservice.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jnsdev.microservices.currencyexchangeservice.bean.CurrencyExchange;
+
+@RestController
+public class CurrencyExchangeController {
+
+      @Autowired
+      private Environment environment;
+
+      @GetMapping("/currency-exchange/from/{from}/to/{to}")
+      public CurrencyExchange retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to
+      ) {
+
+            CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+            
+            String port = environment.getProperty("local.server.port");
+            
+            currencyExchange.setEnvironment(port);
+            return currencyExchange;
+      }
+
+      
+}
---
+++ bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO currency_exchange(
+            id,
+            currency_from,
+            currency_to,
+            conversion_multiple,
+            environment
+      )
+VALUES(1001, 'USD', 'INR', 65, '');
+
+INSERT INTO currency_exchange(
+            id,
+            currency_from,
+            currency_to,
+            conversion_multiple,
+            environment
+      )
+VALUES(1002, 'EUR', 'INR', 80, '');
+
+INSERT INTO currency_exchange(
+            id,
+            currency_from,
+            currency_to,
+            conversion_multiple,
+            environment
+      )
+VALUES(1003, 'AUD', 'INR', 100, '');
