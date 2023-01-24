5a6427709467ce82b06b8d07bc6216946e7211ba
Jairo Nascimento <jaironsousa@gmail.com>
2023-01-24 08:40:52 -0300

    Curso Master Microservices with Spring Cloud - V2  - Seção: 6 Restfull webservices  	- aula: 164 Step 29 - Exploring Rate Limiting and BulkHead Features of Resilience4j - V2

--- bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-exchange-service/src/main/java/com/jnsdev/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-exchange-service/src/main/java/com/jnsdev/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -3,7 +3,9 @@ package com.jnsdev.microservices.currencyexchangeservice.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 import org.slf4j.Logger;
@@ -17,7 +19,9 @@ public class CircuitBreakerController {
       private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
       // @Retry(name = "sample-api", fallbackMethod = "hardcodeResponse")
-      @CircuitBreaker(name = "default", fallbackMethod = "hardcodeResponse")
+      // @CircuitBreaker(name = "default", fallbackMethod = "hardcodeResponse")
+      // @RateLimiter(name = "default")
+      @Bulkhead(name = "default")
       @GetMapping(value = "/sample-api")
       public String sampleApi() {
 
