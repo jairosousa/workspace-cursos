62133dac64bb89b9710edfbf7fc081c3ec80b909
Jairo Nascimento <jaironsousa@gmail.com>
2023-01-24 06:47:35 -0300

    Curso Master Microservices with Spring Cloud - V2  - Seção: 6 Restfull webservices  	- aula: 161 Step 27 - Playing with Resilience4j - Retry and Fallback Methods

--- bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-exchange-service/src/main/java/com/jnsdev/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-exchange-service/src/main/java/com/jnsdev/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,15 +1,32 @@
 package com.jnsdev.microservices.currencyexchangeservice.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class CircuitBreakerController {
 
+      private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+      @Retry(name = "sample-api", fallbackMethod = "hardcodeResponse")
       @GetMapping(value = "/sample-api")
       public String sampleApi() {
-            return "Sample API";
+
+            logger.info("Sample Apli call received");
+            ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+                        String.class);
+            return forEntity.getBody();
+      }
+
+      public String hardcodeResponse(Exception ex) {
+            return "fallback-response";
       }
 
 }
