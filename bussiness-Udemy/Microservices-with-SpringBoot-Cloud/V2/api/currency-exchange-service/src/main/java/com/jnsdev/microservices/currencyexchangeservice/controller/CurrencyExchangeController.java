925144c26d00084d24209a04ae0aeb7c7f957e3c
Jairo Nascimento <jaironsousa@gmail.com>
2023-01-24 11:58:45 -0300

    Curso Master Microservices with Spring Cloud - V2  - Seção: 6 Restfull webservices  	- aula: 178 Step 12 - Connecting Currency Exchange Microservice with Zipkin

--- bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-exchange-service/src/main/java/com/jnsdev/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ bussiness-Udemy/Microservices-with-SpringBoot-Cloud/V2/api/currency-exchange-service/src/main/java/com/jnsdev/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.jnsdev.microservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ import com.jnsdev.microservices.currencyexchangeservice.repository.CurrencyExcha
 @RestController
 public class CurrencyExchangeController {
 
+      private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
       @Autowired
       private CurrencyExchangeRepository currencyExchangeRepository;
 
@@ -24,6 +28,8 @@ public class CurrencyExchangeController {
       public CurrencyExchange retrieveExchangeValue(
                   @PathVariable String from,
                   @PathVariable String to) {
+
+            logger.info("retrieveExchangeValue called with {} to {}", from, to);
 
             CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
 
